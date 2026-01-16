f35ec635df00ce9b0e5d69fb814fef9e52308885
fadrian06 <franyeradriansanchez@gmail.com>
2026-01-16 03:56:09 -0400

    bind Container and Flight

--- index.php
+++ index.php
@@ -6,4 +6,6 @@ use Leaf\Auth;
 require __DIR__ . '/vendor/autoload.php';
 
 Container::getInstance()->singleton(Auth::class);
+Flight::registerContainerHandler(Container::getInstance());
+
 Flight::start();
