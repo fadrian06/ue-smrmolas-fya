ee63ce0cffb6afd6d7c581864de300556ff42181
fadrian06 <franyeradriansanchez@gmail.com>
2026-01-16 05:43:07 -0400

    create basic users table

--- index.php
+++ index.php
@@ -14,6 +14,18 @@ if (!file_exists('.env')) {
 
 Container::getInstance()->singleton(Auth::class);
 Container::getInstance()->get(Auth::class)->config('session', true);
+Container::getInstance()
+  ->get(Auth::class)
+  ->autoConnect()
+  ->db()
+  ->query(<<<'sql'
+    create table if not exists users (
+      id integer,
+      email varchar(255),
+      password varchar(255)
+    );
+  sql)
+  ->execute();
 Flight::registerContainerHandler(Container::getInstance());
 
 Flight::start();
