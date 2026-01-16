c92f0286200efb66d3d3e3c067d3524ece98a54f
fadrian06 <franyeradriansanchez@gmail.com>
2026-01-16 04:00:01 -0400

    add .env loading

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 
 use flight\Container;
 use Leaf\Auth;
+use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
+
+if (!file_exists('.env')) {
+  copy('.env.example', '.env');
+}
+
+(new Dotenv)->load('.env.example', '.env');
 
 Container::getInstance()->singleton(Auth::class);
 Flight::registerContainerHandler(Container::getInstance());
