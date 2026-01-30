f338815cfe8efe52d655a63fe9d1df2520c2ea44
fadrian06 <franyeradriansanchez@gmail.com>
2026-01-30 01:02:29 -0400

    replace $_ENV with \Leaf\_env in oauth2.php

--- routes/oauth2.php
+++ routes/oauth2.php
@@ -5,13 +5,15 @@ use flight\Container;
 use Leaf\Auth;
 use League\OAuth2\Client\Provider\Google;
 
+use function Leaf\_env;
+
 $google = Container::getInstance()->get(Auth::class)->client('google');
 
 if ($google instanceof Google) {
-  $redirectUri = (string) (new ReflectionProperty($google, 'redirectUri'))->getValue($google);
+  $redirectUri = (new ReflectionProperty($google, 'redirectUri'))->getValue($google);
 
   Flight::route(
-    'GET ' . str_replace((string) $_ENV['APP_URL'], '', $redirectUri),
+    'GET ' . str_replace((string) _env('APP_URL'), '', (string) $redirectUri),
     [GoogleController::class, 'handleCallback'],
     alias: 'oauth2.google.callback',
   );
