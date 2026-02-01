bc12896547fb0fcac9b2239a40264e0e3cd34303
fadrian06 <franyeradriansanchez@gmail.com>
2026-02-01 12:41:01 -0400

    clean sidebar

--- resources/views/components/sidebar.php
+++ resources/views/components/sidebar.php
@@ -1,210 +1,276 @@
+<?php
+
+$sidebarNavItems = [
+  [
+    'href' => './',
+    'title' => 'Dashboard',
+    'flaticonIcon' => 'dashboard',
+  ],
+  // [
+  //   'title' => 'Students',
+  //   'flaticonIcon' => 'classmates',
+  //   'subGroupMenuItems' => [
+  //     [
+  //       'href' => './all-student',
+  //       'title' => 'All Students',
+  //     ],
+  //     [
+  //       'href' => './student-details',
+  //       'title' => 'Student Details',
+  //     ],
+  //     [
+  //       'href' => './admit-form',
+  //       'title' => 'Admission Form',
+  //     ],
+  //     [
+  //       'href' => './student-promotion',
+  //       'title' => 'Student Promotion',
+  //     ],
+  //   ],
+  // ],
+  // [
+  //   'title' => 'Teachers',
+  //   'flaticonIcon' => 'multiple-users-silhouette',
+  //   'subGroupMenuItems' => [
+  //     [
+  //       'href' => './all-teacher',
+  //       'title' => 'All Teachers',
+  //     ],
+  //     [
+  //       'href' => './teacher-details',
+  //       'title' => 'Teacher Details',
+  //     ],
+  //     [
+  //       'href' => './add-teacher',
+  //       'title' => 'Add Teacher',
+  //     ],
+  //     [
+  //       'href' => './teacher-payment',
+  //       'title' => 'Payment',
+  //     ],
+  //   ],
+  // ],
+  // [
+  //   'title' => 'Parents',
+  //   'flaticonIcon' => 'couple',
+  //   'subGroupMenuItems' => [
+  //     [
+  //       'href' => './all-parents',
+  //       'title' => 'All Parents',
+  //     ],
+  //     [
+  //       'href' => './parents-details',
+  //       'title' => 'Parents Details',
+  //     ],
+  //     [
+  //       'href' => './add-parents',
+  //       'title' => 'Add Parent',
+  //     ],
+  //   ],
+  // ],
+  // [
+  //   'title' => 'Library',
+  //   'flaticonIcon' => 'books',
+  //   'subGroupMenuItems' => [
+  //     [
+  //       'href' => './all-book',
+  //       'title' => 'All Book',
+  //     ],
+  //     [
+  //       'href' => './add-book',
+  //       'title' => 'Add New Book',
+  //     ],
+  //   ],
+  // ],
+  // [
+  //   'title' => 'Acconunt',
+  //   'flaticonIcon' => 'technological',
+  //   'subGroupMenuItems' => [
+  //     [
+  //       'href' => './all-fees',
+  //       'title' => 'All Fees Collection',
+  //     ],
+  //     [
+  //       'href' => './all-expense',
+  //       'title' => 'Expenses',
+  //     ],
+  //     [
+  //       'href' => './add-expense',
+  //       'title' => 'Add Expenses',
+  //     ],
+  //   ],
+  // ],
+  // [
+  //   'title' => 'Class',
+  //   'flaticonIcon' => 'maths-class-materials-cross-of-a-pencil-and-a-ruler',
+  //   'subGroupMenuItems' => [
+  //     [
+  //       'href' => './all-class',
+  //       'title' => 'All Classes',
+  //     ],
+  //     [
+  //       'href' => './add-class',
+  //       'title' => 'Add New Class',
+  //     ],
+  //   ],
+  // ],
+  // [
+  //   'href' => './all-subject',
+  //   'title' => 'Subject',
+  //   'flaticonIcon' => 'open-book',
+  // ],
+  // [
+  //   'href' => './class-routine',
+  //   'title' => 'Class Routine',
+  //   'flaticonIcon' => 'calendar',
+  // ],
+  // [
+  //   'href' => './student-attendence',
+  //   'title' => 'Attendence',
+  //   'flaticonIcon' => 'checklist',
+  // ],
+  // [
+  //   'title' => 'Exam',
+  //   'flaticonIcon' => 'shopping-list',
+  //   'subGroupMenuItems' => [
+  //     [
+  //       'href' => './exam-schedule',
+  //       'title' => 'Exam Schedule',
+  //     ],
+  //     [
+  //       'href' => './exam-grade',
+  //       'title' => 'Exam Grades',
+  //     ],
+  //   ],
+  // ],
+  // [
+  //   'href' => './transport',
+  //   'title' => 'Transport',
+  //   'flaticonIcon' => 'bus-side-view',
+  // ],
+  // [
+  //   'href' => './hostel',
+  //   'title' => 'Hostel',
+  //   'flaticonIcon' => 'bed',
+  // ],
+  // [
+  //   'href' => './notice-board',
+  //   'title' => 'Notice Board',
+  //   'flaticonIcon' => 'script',
+  // ],
+  // [
+  //   'href' => './messaging',
+  //   'title' => 'Messaging',
+  //   'flaticonIcon' => 'chat',
+  // ],
+  // [
+  //   'title' => 'UI Elements',
+  //   'flaticonIcon' => 'menu-1',
+  //   'subGroupMenuItems' => [
+  //     [
+  //       'href' => './notification-alart',
+  //       'title' => 'Alart',
+  //     ],
+  //     [
+  //       'href' => './button',
+  //       'title' => 'Button',
+  //     ],
+  //     [
+  //       'href' => './grid',
+  //       'title' => 'Grid',
+  //     ],
+  //     [
+  //       'href' => './modal',
+  //       'title' => 'Modal',
+  //     ],
+  //     [
+  //       'href' => './progress-bar',
+  //       'title' => 'Progress Bar',
+  //     ],
+  //     [
+  //       'href' => './ui-tab',
+  //       'title' => 'Tab',
+  //     ],
+  //     [
+  //       'href' => './ui-widget',
+  //       'title' => 'Widget',
+  //     ],
+  //   ],
+  // ],
+  // [
+  //   'href' => './map',
+  //   'title' => 'Map',
+  //   'flaticonIcon' => 'planet-earth',
+  // ],
+  [
+    'href' => './account-settings',
+    'title' => 'Cuenta',
+    'flaticonIcon' => 'settings',
+  ]
+];
+
+$requestUrl = '.' . Flight::request()->url;
+
+?>
+
 <!-- Sidebar Area Start Here -->
 <div class="sidebar-main sidebar-menu-one sidebar-expand-md sidebar-color">
   <div class="mobile-sidebar-header d-md-none">
     <div class="header-logo">
-      <a href="index.html"><img src="./resources/img/logo1.png" alt="logo"></a>
+      <a href="./">
+        <img src="./resources/img/logo1.png" />
+      </a>
     </div>
   </div>
   <div class="sidebar-menu-content">
     <ul class="nav nav-sidebar-menu sidebar-toggle-view">
-      <li class="nav-item sidebar-nav-item">
-        <a href="#" class="nav-link"><i class="flaticon-dashboard"></i><span>Dashboard</span></a>
-        <ul class="nav sub-group-menu">
-          <li class="nav-item">
-            <a href="index.html" class="nav-link"><i class="fas fa-angle-right"></i>Admin</a>
+      <?php foreach ($sidebarNavItems as $sidebarNavItem) : ?>
+        <?php
+
+        $sidebarNavItem['href'] ??= '';
+        $sidebarNavItem['subGroupMenuItems'] ??= [];
+
+        $sidebarNavItem['isActive'] = $requestUrl === $sidebarNavItem['href'] || array_any(
+          array_map(
+            fn(array $subGroupMenuItem): string => (string) $subGroupMenuItem['href'] ?? '',
+            (array) $sidebarNavItem['subGroupMenuItems'],
+          ),
+          fn(string $href): bool => $requestUrl === $href,
+        );
+
+        ?>
+        <?php if ($sidebarNavItem['subGroupMenuItems']) : ?>
+          <li class="nav-item sidebar-nav-item <?= !$sidebarNavItem['isActive'] ?: 'active' ?>">
+            <a href="#" class="nav-link">
+              <i class="flaticon-<?= $sidebarNavItem['flaticonIcon'] ?? '' ?>"></i>
+              <span><?= $sidebarNavItem['title'] ?? '' ?></span>
+            </a>
+            <ul
+              class="nav sub-group-menu <?= !$sidebarNavItem['isActive'] ?: 'menu-open' ?>"
+              style="<?= !$sidebarNavItem['isActive'] ?: 'display: block' ?>">
+              <?php foreach ((array) $sidebarNavItem['subGroupMenuItems'] ?? [] as $subGroupMenuItem) : ?>
+                <li class="nav-item">
+                  <a
+                    href="<?= $subGroupMenuItem['href'] ?? '#' ?>"
+                    class="nav-link <?= ($requestUrl) !== $subGroupMenuItem['href'] ?? '' ?: 'menu-active' ?>">
+                    <i class="fas fa-angle-right"></i>
+                    <?= $subGroupMenuItem['title'] ?? '' ?>
+                  </a>
+                </li>
+              <?php endforeach ?>
+            </ul>
           </li>
+        <?php else : ?>
           <li class="nav-item">
-            <a href="index3.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Students</a>
+            <a
+              href="<?= $sidebarNavItem['href'] ?? '' ?>"
+              class="nav-link <?= !$sidebarNavItem['isActive'] ?: 'menu-active' ?>">
+              <i class="flaticon-<?= $sidebarNavItem['flaticonIcon'] ?? '' ?>"></i>
+              <span><?= $sidebarNavItem['title'] ?? '' ?></span>
+            </a>
           </li>
-          <li class="nav-item">
-            <a href="index4.html" class="nav-link"><i class="fas fa-angle-right"></i>Parents</a>
-          </li>
-          <li class="nav-item">
-            <a href="index5.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Teachers</a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item sidebar-nav-item">
-        <a href="#" class="nav-link"><i class="flaticon-classmates"></i><span>Students</span></a>
-        <ul class="nav sub-group-menu">
-          <li class="nav-item">
-            <a href="all-student.html" class="nav-link"><i class="fas fa-angle-right"></i>All
-              Students</a>
-          </li>
-          <li class="nav-item">
-            <a href="student-details.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Student Details</a>
-          </li>
-          <li class="nav-item">
-            <a href="admit-form.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Admission Form</a>
-          </li>
-          <li class="nav-item">
-            <a href="student-promotion.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Student Promotion</a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item sidebar-nav-item">
-        <a href="#" class="nav-link"><i
-            class="flaticon-multiple-users-silhouette"></i><span>Teachers</span></a>
-        <ul class="nav sub-group-menu">
-          <li class="nav-item">
-            <a href="all-teacher.html" class="nav-link"><i class="fas fa-angle-right"></i>All
-              Teachers</a>
-          </li>
-          <li class="nav-item">
-            <a href="teacher-details.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Teacher Details</a>
-          </li>
-          <li class="nav-item">
-            <a href="add-teacher.html" class="nav-link"><i class="fas fa-angle-right"></i>Add
-              Teacher</a>
-          </li>
-          <li class="nav-item">
-            <a href="teacher-payment.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Payment</a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item sidebar-nav-item">
-        <a href="#" class="nav-link"><i class="flaticon-couple"></i><span>Parents</span></a>
-        <ul class="nav sub-group-menu">
-          <li class="nav-item">
-            <a href="all-parents.html" class="nav-link"><i class="fas fa-angle-right"></i>All
-              Parents</a>
-          </li>
-          <li class="nav-item">
-            <a href="parents-details.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Parents Details</a>
-          </li>
-          <li class="nav-item">
-            <a href="add-parents.html" class="nav-link"><i class="fas fa-angle-right"></i>Add
-              Parent</a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item sidebar-nav-item">
-        <a href="#" class="nav-link"><i class="flaticon-books"></i><span>Library</span></a>
-        <ul class="nav sub-group-menu">
-          <li class="nav-item">
-            <a href="all-book.html" class="nav-link"><i class="fas fa-angle-right"></i>All
-              Book</a>
-          </li>
-          <li class="nav-item">
-            <a href="add-book.html" class="nav-link"><i class="fas fa-angle-right"></i>Add New
-              Book</a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item sidebar-nav-item">
-        <a href="#" class="nav-link"><i class="flaticon-technological"></i><span>Acconunt</span></a>
-        <ul class="nav sub-group-menu">
-          <li class="nav-item">
-            <a href="all-fees.html" class="nav-link"><i class="fas fa-angle-right"></i>All Fees
-              Collection</a>
-          </li>
-          <li class="nav-item">
-            <a href="all-expense.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Expenses</a>
-          </li>
-          <li class="nav-item">
-            <a href="add-expense.html" class="nav-link"><i class="fas fa-angle-right"></i>Add
-              Expenses</a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item sidebar-nav-item">
-        <a href="#" class="nav-link"><i
-            class="flaticon-maths-class-materials-cross-of-a-pencil-and-a-ruler"></i><span>Class</span></a>
-        <ul class="nav sub-group-menu">
-          <li class="nav-item">
-            <a href="all-class.html" class="nav-link"><i class="fas fa-angle-right"></i>All
-              Classes</a>
-          </li>
-          <li class="nav-item">
-            <a href="add-class.html" class="nav-link"><i class="fas fa-angle-right"></i>Add New
-              Class</a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item">
-        <a href="all-subject.html" class="nav-link"><i
-            class="flaticon-open-book"></i><span>Subject</span></a>
-      </li>
-      <li class="nav-item">
-        <a href="class-routine.html" class="nav-link"><i class="flaticon-calendar"></i><span>Class
-            Routine</span></a>
-      </li>
-      <li class="nav-item">
-        <a href="student-attendence.html" class="nav-link"><i
-            class="flaticon-checklist"></i><span>Attendence</span></a>
-      </li>
-      <li class="nav-item sidebar-nav-item">
-        <a href="#" class="nav-link"><i class="flaticon-shopping-list"></i><span>Exam</span></a>
-        <ul class="nav sub-group-menu">
-          <li class="nav-item">
-            <a href="exam-schedule.html" class="nav-link"><i class="fas fa-angle-right"></i>Exam
-              Schedule</a>
-          </li>
-          <li class="nav-item">
-            <a href="exam-grade.html" class="nav-link"><i class="fas fa-angle-right"></i>Exam
-              Grades</a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item">
-        <a href="transport.html" class="nav-link"><i
-            class="flaticon-bus-side-view"></i><span>Transport</span></a>
-      </li>
-      <li class="nav-item">
-        <a href="hostel.html" class="nav-link"><i class="flaticon-bed"></i><span>Hostel</span></a>
-      </li>
-      <li class="nav-item">
-        <a href="notice-board.html" class="nav-link"><i
-            class="flaticon-script"></i><span>Notice</span></a>
-      </li>
-      <li class="nav-item">
-        <a href="messaging.html" class="nav-link"><i
-            class="flaticon-chat"></i><span>Messeage</span></a>
-      </li>
-      <li class="nav-item sidebar-nav-item">
-        <a href="#" class="nav-link"><i class="flaticon-menu-1"></i><span>UI Elements</span></a>
-        <ul class="nav sub-group-menu">
-          <li class="nav-item">
-            <a href="notification-alart.html" class="nav-link"><i class="fas fa-angle-right"></i>Alart</a>
-          </li>
-          <li class="nav-item">
-            <a href="button.html" class="nav-link"><i class="fas fa-angle-right"></i>Button</a>
-          </li>
-          <li class="nav-item">
-            <a href="grid.html" class="nav-link"><i class="fas fa-angle-right"></i>Grid</a>
-          </li>
-          <li class="nav-item">
-            <a href="modal.html" class="nav-link"><i class="fas fa-angle-right"></i>Modal</a>
-          </li>
-          <li class="nav-item">
-            <a href="progress-bar.html" class="nav-link"><i class="fas fa-angle-right"></i>Progress Bar</a>
-          </li>
-          <li class="nav-item">
-            <a href="ui-tab.html" class="nav-link"><i class="fas fa-angle-right"></i>Tab</a>
-          </li>
-          <li class="nav-item">
-            <a href="ui-widget.html" class="nav-link"><i
-                class="fas fa-angle-right"></i>Widget</a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item">
-        <a href="map.html" class="nav-link"><i
-            class="flaticon-planet-earth"></i><span>Map</span></a>
-      </li>
-      <li class="nav-item">
-        <a href="account-settings.html" class="nav-link"><i
-            class="flaticon-settings"></i><span>Account</span></a>
-      </li>
+        <?php endif ?>
+      <?php endforeach ?>
     </ul>
   </div>
 </div>
